542ce31da277f58b0d556cefefdde57b14fb754e
briggirl2 <briggirl2@gmail.com>
2020-01-18 02:01:15 +0900

    11. DB sheme management using migrations

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,8 @@ from shop.converters import FourDigitYearConverter
 
 register_converter(FourDigitYearConverter, 'yyyy')
 
+app_name = 'shop'
+
 urlpatterns = [
-    path('archives/<yyyy:year>/', views.archives_year)
+    path('archives/<yyyy:year>/', views.archives_year),
 ]
