c75d879872ad5b4d81b48de13c9b4887f56063d1
briggirl2 <briggirl2@gmail.com>
2020-01-21 00:26:38 +0900

    19. Django Template Language

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'askcompany', 'templates')
+            os.path.join(BASE_DIR, 'askcompany', 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
